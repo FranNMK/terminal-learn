b8a9910eda8b71db730aa9e2a14bd3eebcef5931
Francis <frankmk2025@gmail.com>
2025-11-25 22:35:33 +0300

    auth: password reset and welcome email

--- termlearn/accounts/signals.py
+++ termlearn/accounts/signals.py
@@ -2,8 +2,26 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def send_welcome_email(sender, instance, created, **kwargs):
+    if created:  # Only send email when a new user is created
+        send_mail(
+            'Welcome to TerminalLearn',
+            f'Hi {instance.first_name}, welcome to TerminalLearn — your place to learn terminal commands!',
+            'noreply@terminallearn.local',  # DEFAULT_FROM_EMAIL also works here
+            [instance.email],
+            fail_silently=True
+        )
--- termlearn/termlearn/settings.py
+++ termlearn/termlearn/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email Settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'noreply@terminallearn.local'
